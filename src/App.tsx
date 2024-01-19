b2c41c1b1ebb471d221c5b10ac31b89ffcf9da61
Gian Putallaz <gianputallaz@gmail.com>
2024-01-19 03:09:10 -0300

    added colors to tailwind safelist classes; add missing keys

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,10 @@ function App() {
     return (
         <div className='App flex'>
             {colors.map((color) => (
-                <div className='h-screen w-1/2'>
+                <div className='h-screen w-1/2' key={color}>
                     <p className='text-xl text-center font-bold'>{color.toUpperCase()}</p>
                     {[...Array(9)].map((_, idx) => (
-                        <Color colorClass={`bg-${color}-${(idx + 1) * 100}`} shade={(idx + 1) * 100} />
+                        <Color colorClass={`bg-${color}-${(idx + 1) * 100}`} shade={(idx + 1) * 100} key={idx} />
                     ))}
                 </div>
             ))}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content:["./src/**/*.{js,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('./node_modules/tailwind-percentage-heights-plugin')(),
-  ]
+    content: ['./src/**/*.{js,jsx,tsx}'],
+    safelist: [
+        {
+            pattern:
+                /bg-(gray|neutral|red|orange|yellow|green|emerald|sky|blue|purple|pink)-(100|200|300|400|500|600|700|800|900)/,
+        },
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [require('./node_modules/tailwind-percentage-heights-plugin')()],
 }
